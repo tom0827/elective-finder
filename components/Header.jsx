2ac45a28bbce94a9928681b14569f97fa7813f35
tom0827 <tchapman000@gmail.com>
2023-03-23 01:31:32 -0700

    Added toggle to select offered courses only

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header(props) {
     return (
         <>
-            <header className="h-20 flex px-10 shadow-md sticky top-0 bg-slate-800">
-                <div className="grid grid-cols-4 w-full">
-                    <div className="flex items-center justify-center text-3xl text-slate-200 font-semibold">Elective Finder</div>
-                    <div className="flex items-center justify-center ">
+            <header className="flex items-center h-20 px-10 shadow-md sticky top-0 bg-slate-800">
+                <div className="flex justify-around grid grid-cols-5 w-full">
+                    <div className=" text-3xl text-slate-200 font-semibold">Elective Finder</div>
+                    <div className="flex items-center">
                         <div className="pr-2 text-xl text-slate-200">
                             {/* eslint-disable-next-line react/no-unescaped-entities */}
                             I'm in
@@ -23,7 +23,7 @@ export default function Header(props) {
                             engineering
                         </div>
                     </div>
-                    <div className="flex items-center justify-center ">
+                    <div className="flex items-center">
                         <div className="pr-2 text-xl text-slate-200">
                             Looking for
                         </div>
@@ -39,7 +39,7 @@ export default function Header(props) {
                             electives
                         </div>
                     </div>
-                    <div className="flex items-center justify-center ">
+                    <div className="flex items-center">
                         <div className="pr-2 text-xl text-slate-200">
                             Term
                         </div>
@@ -49,6 +49,15 @@ export default function Header(props) {
                                 }>
                             <option value="202305">Summer 2023</option>
                         </select>
+                    </div>
+                    <div className="flex items-center">
+                        <label className="relative inline-flex items-center cursor-pointer">
+                            <input type="checkbox" value="" className="sr-only peer" checked={props.offeredCourses} onChange={() => props.setOfferedCourses(!props.offeredCourses)} />
+                                <div className="w-14 h-7 bg-slate-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-slate-600 dark:peer-focus:ring-slate-600 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-slate-500"></div>
+                                <span className="ml-3 text-xl text-slate-200 dark:text-gray-300">
+                                    Offered Courses
+                                </span>
+                        </label>
                     </div>
                 </div>
             </header>
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
     const [program, setProgram] = useState("Software");
     const [electiveType, setElectiveType] = useState("Technical");
     const [selectedTerm, setSelectedTerm] = useState("202305");
+    const [offeredCourses, setOfferedCourses] = useState(true);
     const [courses, setCourses] = useState([])
 
     useEffect(() => {
@@ -28,26 +29,28 @@ export default function Home() {
     const renderCourseList = courses.map(course =>
         {
             const courseName = Object.keys(course)[1]
-            if(course[courseName]['for'].includes(program))
-            {
-                return (
-                    <div key={courseName} className="bg-slate-300 p-3 rounded-2xl">
-                        <div className="flex flex-row items-center">
-                            <div className="font-bold text-2xl">{courseName}</div>
-                            &nbsp;
-                            <div className="font-semibold text-xl">-&nbsp;{course[courseName]['title']}</div>
-                        </div>
-                        <div>
-                            <div className="flex flex-row">
-                                Offered {termMap[selectedTerm]}: &nbsp; {course[courseName]['is_offered'] ? <div>Yes</div> : <div>No</div> }
-                            </div>
-                            <div>
-                                {course[courseName]['description']}
-                            </div>
-                        </div>
+
+            if(!course[courseName]['for'].includes(program)) return
+
+            if(offeredCourses === true && course[courseName]['is_offered'] !== true) return
+
+            return (
+                <div key={courseName} className="flex justify-center text-center bg-slate-700 p-3 rounded-2xl text-slate-200 cursor-pointer hover:bg-slate-800">
+                    <div className="flex flex-row items-center">
+                        <div className="font-bold text-2xl">{courseName}</div>
+                        {/*&nbsp;*/}
+                        {/*<div className="font-semibold text-xl">-&nbsp;{course[courseName]['title']}</div>*/}
                     </div>
-                )
-            }
+                    {/*<div>*/}
+                    {/*    <div className="flex flex-row">*/}
+                    {/*        Offered {termMap[selectedTerm]}: &nbsp; {course[courseName]['is_offered'] ? <div>Yes</div> : <div>No</div> }*/}
+                    {/*    </div>*/}
+                    {/*    <div>*/}
+                    {/*        { 'description' in course[courseName] ? <div>{course[courseName]['description']}</div> : <div>No description available</div>}*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                </div>
+            )
 
         }
     );
@@ -64,9 +67,11 @@ export default function Home() {
               setElectiveType={setElectiveType}
               selectedTerm={selectedTerm}
               setSelectedTerm={setSelectedTerm}
+              offeredCourses={offeredCourses}
+              setOfferedCourses={setOfferedCourses}
           />
-          <main className="bg-slate-600">
-              <div className="pt-20 px-40 grid grid-cols-3 gap-5">
+          <main className="bg-slate-500 h-screen">
+              <div className="bg-slate-500 py-20 px-40 grid grid-cols-4 gap-x-44 gap-y-12">
                   {renderCourseList}
               </div>
           </main>
