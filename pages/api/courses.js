fff86d4b354ce968daf410a50889590dc73f0c93
tom0827 <tchapman000@gmail.com>
2023-03-24 22:12:48 -0700

    Added query params

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -5,7 +5,10 @@ export default async function handler(req, res) {
     const client = await clientPromise
     const db = client.db("uvic_electives")
 
-    const courses = await db.collection("technical_electives.202305").find({}).toArray()
+    const query = req.query;
+    let { term, type } = query;
+
+    const courses = await db.collection(`${type}_electives.${term}`.toLowerCase()).find({}).toArray()
     res.status(200).json(courses)
   } catch (e) {
     res.status(200).json(e)
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import {useEffect, useState} from "react";
 import CourseTable from "@/components/CourseTable";
+import {useRouter} from "next/router";
 
 // const termMap = {'202305': 'Summer 2023'}
 
 export default function Home() {
+    const router = useRouter();
     const [program, setProgram] = useState("Software");
     const [electiveType, setElectiveType] = useState("Technical");
     const [selectedTerm, setSelectedTerm] = useState("202305");
@@ -14,9 +16,13 @@ export default function Home() {
     const [courses, setCourses] = useState([])
 
     useEffect(() => {
+        router.query.term = selectedTerm
+        router.query.type = electiveType
+        router.push(router)
         getCourses()
             .then((response) => {
                 setCourses(response)
+                console.log(response)
             })
             .catch((error) => {
                 console.log(error)
@@ -24,7 +30,7 @@ export default function Home() {
     }, [electiveType, selectedTerm])
 
     async function getCourses() {
-        const response = await fetch('/api/courses')
+        const response = await fetch(`/api/courses?term=${router.query.term}&type=${router.query.type}`)
         return response.json()
     }
 
