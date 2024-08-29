09b1bca03f7b3f31171a43ff4ed914d965049372
tom0827 <tchapman000@gmail.com>
2024-08-29 15:16:04 -0700

    remove uneeded route

--- src/app/api/courses/route.ts
+++
@@ -1,7 +0,0 @@
-import { getCourses } from "../../../../supabase/courses";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const courses = await getCourses();
-  return NextResponse.json(courses);
-};
