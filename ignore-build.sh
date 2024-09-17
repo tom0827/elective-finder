277ba0ee49518e2b5cecb7ab22717733e7c5cc24
Tom Chapman <tchapman000@gmail.com>
2024-09-16 17:52:32 -0700

    testing new ignore-build file

--- ignore-build.sh
+++ ignore-build.sh
@@ -2,13 +2,12 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
-  # Proceed with the build
-    echo "✅ - Build can proceed"
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "staging"  ]] ; then
+    echo "Build can proceed"
   exit 1;
 
 else
   # Don't build
-  echo "🛑 - Build cancelled"
+  echo "Build cancelled"
   exit 0;
 fi
